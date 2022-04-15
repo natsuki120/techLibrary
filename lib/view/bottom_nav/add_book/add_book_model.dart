c430d88dbdd505de90529151d4bf10814c041b10
natsuki <synchro0718@icloud.com>
2022-04-15 15:09:54 +0900

    本の登録

---
+++ lib/view/bottom_nav/add_book/add_book_model.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class AddBookModel extends ChangeNotifier {
+  String selectedGenre = 'フロントエンド';
+  String? title;
+  String? author;
+  File? imageFile;
+  final picker = ImagePicker();
+
+  Future addBook() async {
+    final doc = FirebaseFirestore.instance.collection('book').doc();
+
+    String? imgURL;
+    if (imageFile != null) {
+      final task = await FirebaseStorage.instance
+          .ref('books/${doc.id}')
+          .putFile(imageFile!);
+      imgURL = await task.ref.getDownloadURL();
+    }
+
+    await doc.set({
+      'title': title,
+      'author': author,
+      'imgURL': imgURL,
+      'genre': selectedGenre,
+    });
+  }
+
+  Future pickImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      imageFile = File(pickedFile.path);
+      notifyListeners();
+    }
+  }
+}
