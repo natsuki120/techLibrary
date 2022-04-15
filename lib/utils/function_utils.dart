e04e73595bc16625861fd566c88cd2fecf3505c1
natsuki <synchro0718@icloud.com>
2022-04-15 15:26:24 +0900

    ユーザーの画像の登録

---
+++ lib/utils/function_utils.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+
+class FunctionUtils {
+  static Future<dynamic> getImageFromGalley() async {
+    ImagePicker picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+    return pickedFile;
+  }
+
+  static Future<String> uploadImage(String uid, File image) async {
+    final FirebaseStorage storageInstance = FirebaseStorage.instance;
+    final Reference ref = storageInstance.ref();
+    await ref.child(uid).putFile(image);
+    String downloadUrl = await storageInstance.ref(uid).getDownloadURL();
+    return downloadUrl;
+  }
+}
