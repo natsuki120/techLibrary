ca88c94d6bbd2722771ff2d7a3d43d26fe06a002
natsuki <synchro0718@icloud.com>
2022-05-01 13:31:31 +0900

    コンポーネント化

---
+++ lib/view/start_up/login/components/registration.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import '../../create_account_page.dart';
+
+class Registration extends StatelessWidget {
+  const Registration({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.only(top: size.height * 0.05),
+      child: RichText(
+        text: TextSpan(
+          style: const TextStyle(color: Colors.black),
+          children: [
+            const TextSpan(
+              text: '新規登録は',
+              style: TextStyle(color: Colors.grey),
+            ),
+            TextSpan(
+              text: 'こちら',
+              style: const TextStyle(color: Colors.blue),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const CreateAccountPage(),
+                    ),
+                  );
+                },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
