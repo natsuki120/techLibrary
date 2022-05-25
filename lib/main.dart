7ca60eaa963e2f5e3d0c42f9e47f425d686dd0ee
natsuki <synchro0718@icloud.com>
2022-05-25 15:41:32 +0900

    riverpodの除去とfirebase_optionの導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:tech_library/model/book_model.dart';
-import 'package:tech_library/view/menu/mypage/account_model.dart';
-import 'package:tech_library/view/start_up/sign_in/sign_in_model.dart';
-import 'package:tech_library/view/start_up/sign_up/sign_up_model.dart';
-import 'view/start_up/sign_in/sign_in_page.dart';
-
-final signupProvider = ChangeNotifierProvider(((ref) => SignUpModel()));
-final signinProvider = ChangeNotifierProvider(((ref) => SignInModel()));
-final bookModelProvider = ChangeNotifierProvider(((ref) => BookModel()));
-final accountProvider = ChangeNotifierProvider(((ref) => AccountModel()));
+import 'package:tech_library/views/start_up/sign_in/sign_in_page.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const ProviderScope(child: MyApp()));
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
