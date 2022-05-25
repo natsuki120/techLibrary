438e953275995337a37af62ea016d4d8367ee720
natsuki <synchro0718@icloud.com>
2022-05-25 17:28:52 +0900

    try文の削除

--- lib/views/start_up/sign_in/components/registration_button.dart
+++ lib/views/start_up/sign_in/components/registration_button.dart
@@ -1,13 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:provider/provider.dart';
 import 'package:tech_library/error/firebase_auth_exception_handler.dart';
 import 'package:tech_library/error/show_error_dialog.dart';
-import 'package:tech_library/main.dart';
-import 'package:tech_library/utils/authentication.dart';
-import 'package:tech_library/utils/firestore/users.dart';
-import 'package:tech_library/view/my_home_page.dart';
+import 'package:tech_library/view_models/start_up/sign_in_model.dart';
+import 'package:tech_library/my_home_page.dart';
 
 class RegistrationButton extends StatelessWidget {
   const RegistrationButton({Key? key}) : super(key: key);
@@ -15,7 +13,7 @@ class RegistrationButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Consumer(builder: (context, ref, child) {
+    return Consumer<SignInModel>(builder: (context, model, child) {
       return GestureDetector(
         child: Center(
           child: Container(
@@ -40,33 +38,20 @@ class RegistrationButton extends StatelessWidget {
         ),
         onTap: () async {
           try {
-            ref.read(signinProvider).checkInfo();
             EasyLoading.show(status: 'loading...');
-            var result = await Authentication.emailSignIn(
-              email: ref.read(signinProvider).emailController.text,
-              pass: ref.read(signinProvider).passwordController.text,
-            );
-            if (result is UserCredential) {
-              var _result = await UserFirestore.getUser(result.user!.uid);
-              EasyLoading.showSuccess('Success!');
-              EasyLoading.dismiss();
-              if (_result == true) {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const MyHomePage(),
-                  ),
-                );
-              }
-            } else {
-              EasyLoading.dismiss();
-              final errorMessage =
-                  FirebaseAuthExceptionHandler.exceptionMessage(result);
-              showErrorDialog(context, errorMessage);
-            }
-          } catch (e) {
+            await model.login();
+            EasyLoading.showSuccess('Success!');
             EasyLoading.dismiss();
-            showErrorDialog(context, '$e');
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const MyHomePage(),
+              ),
+            );
+          } on FirebaseAuthException catch (e) {
+            EasyLoading.dismiss();
+            var result = FirebaseAuthExceptionHandler.handleException(e);
+            showErrorDialog(context, result);
           }
         },
       );
