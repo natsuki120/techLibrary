aa7ab1603ca1dcf68d8869d53d1213adfec357b4
natsuki <synchro0718@icloud.com>
2022-05-25 17:50:53 +0900

    ディレクトリ名変更によるimportパスの変更

--- lib/views/mypage/components/my_book.dart
+++ lib/views/mypage/components/my_book.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../../../../utils/firestore/users.dart';
-import '../../../book_detail/return_book.dart';
-import '../account_model.dart';
-import '../all_my_book_page.dart';
+import 'package:tech_library/view_models/account/account_model.dart';
+import 'package:tech_library/views/book_detail/return_book.dart';
+import 'package:tech_library/views/mypage/all_my_book_page.dart';
 
 class MyBook extends StatelessWidget {
   const MyBook({Key? key}) : super(key: key);
@@ -49,10 +48,7 @@ class MyBook extends StatelessWidget {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => const AllMyBookPage(
-                        title: '借りた本一覧',
-                        collection: 'my_book',
-                      ),
+                      builder: (context) => const AllMyBookPage(),
                     ),
                   );
                 },
@@ -64,46 +60,38 @@ class MyBook extends StatelessWidget {
           create: (_) => AccountModel()..getMyBook(),
           child: Consumer<AccountModel>(
             builder: (context, model, child) {
-              final List? usersBook = model.usersBook;
-              if (usersBook == null) {
-                return const Text('借りている本はありません');
-              }
-              final List<Widget> widgets = usersBook.map((book) {
-                return GestureDetector(
-                  child: Container(
-                    margin: EdgeInsets.symmetric(
-                      horizontal: size.width * 0.02,
-                      vertical: size.height * 0.025,
-                    ),
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 10,
-                          color: Colors.black.withOpacity(0.5),
-                        )
-                      ],
-                    ),
-                    child: Image(
-                      image: NetworkImage(book.imgURL),
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                  onTap: () async {
-                    UserFirestore.getMyBook();
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ReturnBook(
-                          bookInfo: book.id,
-                          bookImg: book.imgURL,
-                          bookName: book.title,
-                          author: book.author,
-                        ),
+              final List<Widget> widgets = model.usersBook!.map(
+                (myBook) {
+                  return GestureDetector(
+                    child: Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: size.width * 0.02,
+                        vertical: size.height * 0.025,
                       ),
-                    );
-                  },
-                );
-              }).toList();
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 10,
+                            color: Colors.black.withOpacity(0.5),
+                          )
+                        ],
+                      ),
+                      child: Image(
+                        image: NetworkImage(myBook.imgURL),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    onTap: () async {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ReturnBook(book: myBook),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ).toList();
               return SizedBox(
                 height: size.height * 0.3,
                 child: SingleChildScrollView(
