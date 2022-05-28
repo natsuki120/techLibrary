cae75f917b258970c69e3f74be48ad5762ef696c
natsuki <synchro0718@icloud.com>
2022-05-28 15:52:03 +0900

    Drawerを追加

--- lib/views/mypage/account_page.dart
+++ lib/views/mypage/account_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:new_gradient_app_bar/new_gradient_app_bar.dart';
+import 'package:tech_library/views/mypage/components/drawer_list_page.dart';
 import 'components/my_account.dart';
 import 'components/my_book.dart';
 import 'components/my_favorite_book.dart';
@@ -8,18 +10,24 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const MyAccount(),
-        Expanded(
-          child: ListView(
-            children: const [
-              MyBook(),
-              MyFavoriteBook(),
-            ],
-          ),
-        )
-      ],
+    return Scaffold(
+      appBar: NewGradientAppBar(
+        gradient: LinearGradient(
+          colors: [Colors.blue, Colors.lightBlue.shade200],
+        ),
+        elevation: 0.0,
+      ),
+      drawer: const Drawer(child: DrawerListPage()),
+      body: Column(
+        children: [
+          const MyAccount(),
+          Expanded(
+            child: ListView(
+              children: const [MyBook(), MyFavoriteBook()],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
