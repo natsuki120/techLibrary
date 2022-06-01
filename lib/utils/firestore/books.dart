e6dadd831b5d098c18856aff7aadaf0528900628
natsuki <synchro0718@icloud.com>
2022-06-01 17:18:04 +0900

    必要性がないのでtry文を除去

--- lib/utils/firestore/books.dart
+++ lib/utils/firestore/books.dart
@@ -10,78 +10,50 @@ class BookFirestore {
       _firestoreinstance.collection('borrowBook');
 
   static Future<dynamic> fetchFavoriteBook(Book favoriteBook) async {
-    try {
-      final CollectionReference favorite = _firestoreinstance
-          .collection('users')
-          .doc(Authentication.myAccount!.id)
-          .collection('favorite');
-      favorite.doc(favoriteBook.id).set({
-        'imgURL': favoriteBook.imgURL,
-        'created_time': Timestamp.now(),
-        'title': favoriteBook.title,
-        'author': favoriteBook.author,
-      });
-      print('お気に入り登録完了');
-      return true;
-    } on FirebaseException catch (e) {
-      print('お気に入り登録エラー：　$e');
-      return false;
-    }
+    final CollectionReference favorite = _firestoreinstance
+        .collection('users')
+        .doc(Authentication.myAccount!.id)
+        .collection('favorite');
+    favorite.doc(favoriteBook.id).set({
+      'imgURL': favoriteBook.imgURL,
+      'created_time': Timestamp.now(),
+      'title': favoriteBook.title,
+      'author': favoriteBook.author,
+    });
   }
 
   static Future<dynamic> removeFavoriteBook(String favoriteBook) async {
-    try {
-      final CollectionReference favorite = _firestoreinstance
-          .collection('users')
-          .doc(Authentication.myAccount!.id)
-          .collection('favorite');
-      favorite.doc(favoriteBook).delete();
-      print('お気に入り削除完了');
-      return true;
-    } on FirebaseException catch (e) {
-      print('お気に入り削除エラー：　$e');
-      return false;
-    }
+    final CollectionReference favorite = _firestoreinstance
+        .collection('users')
+        .doc(Authentication.myAccount!.id)
+        .collection('favorite');
+    favorite.doc(favoriteBook).delete();
   }
 
   static Future<dynamic> borrowBook(Book borrowBook) async {
-    try {
-      final CollectionReference _userBooks = _firestoreinstance
-          .collection('users')
-          .doc(Authentication.myAccount!.id)
-          .collection('my_book');
-      _userBooks.doc(borrowBook.id).set({
-        'created_time': Timestamp.now(),
-        'imgURL': borrowBook.imgURL,
-        'title': borrowBook.title,
-        'author': borrowBook.author,
-      });
-      borrowBookCollection
-          .doc(borrowBook.id)
-          .set({'created_time': Timestamp.now()});
-      print('貸出完了');
-      return true;
-    } on FirebaseException catch (e) {
-      print('貸出エラー：　$e');
-      return false;
-    }
+    final CollectionReference _userBooks = _firestoreinstance
+        .collection('users')
+        .doc(Authentication.myAccount!.id)
+        .collection('my_book');
+    _userBooks.doc(borrowBook.id).set({
+      'created_time': Timestamp.now(),
+      'imgURL': borrowBook.imgURL,
+      'title': borrowBook.title,
+      'author': borrowBook.author,
+    });
+    borrowBookCollection
+        .doc(borrowBook.id)
+        .set({'created_time': Timestamp.now()});
   }
 
   static Future<dynamic> returnMyBook(String returnBook) async {
-    try {
-      final DocumentReference<Map<String, dynamic>> _userBooks =
-          _firestoreinstance
-              .collection('users')
-              .doc(Authentication.myAccount!.id)
-              .collection('my_book')
-              .doc(returnBook);
-      _userBooks.delete();
-      borrowBookCollection.doc(returnBook).delete();
-      print('返却完了');
-      return true;
-    } on FirebaseException catch (e) {
-      print('返却エラー：　$e');
-      return false;
-    }
+    final DocumentReference<Map<String, dynamic>> _userBooks =
+        _firestoreinstance
+            .collection('users')
+            .doc(Authentication.myAccount!.id)
+            .collection('my_book')
+            .doc(returnBook);
+    _userBooks.delete();
+    borrowBookCollection.doc(returnBook).delete();
   }
 }
